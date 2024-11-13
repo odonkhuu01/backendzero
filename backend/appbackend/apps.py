dc5fbb177d5927f596ce0d27e48fae417b6f20e9
star.odic <odonkhuu4@gmail.com>
2024-11-13 11:27:42 +0800

    some fixing
    f

--- backend/appbackend/apps.py
+++ backend/appbackend/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ZeroappConfig(AppConfig):
+class AppbackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'zeroapp'
+    name = 'appbackend'
---
+++ backend/backend/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from appbackend import views
+
+urlpatterns = [
+    path('user/', views.checkService), # localhost:8000/user/ gehed views.checkService function duudna.
+    
+]
